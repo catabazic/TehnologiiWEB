023ea35a9d858dfc6afa8f025306f95d5e25fb14
Catalina Bazic <92866393+catabazic@users.noreply.github.com>
2023-05-19 15:00:04 +0300

    Create script.sql

---
+++ database/script.sql
@@ -0,0 +1,40 @@
+-- Create table for Produse
+CREATE TABLE Produse (
+    ID INT PRIMARY KEY,
+    Nume VARCHAR(255),
+    Descriere VARCHAR(255),
+    Pret DECIMAL(10, 2),
+    Cantitate_in_stoc INT
+);
+
+-- Create table for Clienti
+CREATE TABLE Clienti (
+    ID INT PRIMARY KEY,
+    URL VARCHAR(255)
+);
+
+-- Create table for Comenzi
+CREATE TABLE Comenzi (
+    ID INT PRIMARY KEY,
+    Data DATE,
+    Ora TIME,
+    ID_Client INT,
+    Valoarea_totala DECIMAL(10, 2),
+    Status VARCHAR(255),
+    FOREIGN KEY (ID_Client) REFERENCES Clienti(ID)
+);
+
+-- Create table for Comanda_produse
+CREATE TABLE Comanda_produse (
+    id_comanda INT,
+    id_produs INT,
+    FOREIGN KEY (id_comanda) REFERENCES Comenzi(ID),
+    FOREIGN KEY (id_produs) REFERENCES Produse(ID)
+);
+
+-- Create table for Mese
+CREATE TABLE Mese (
+    id INT PRIMARY KEY,
+    numar_locuri INT,
+    ocupate INT
+);
