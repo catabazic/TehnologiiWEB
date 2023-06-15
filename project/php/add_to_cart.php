5c4c4add9fc96fa83065aec803bfb696182e8540
Bazic Catalina <bazic.catalina@gmail.com>
2023-06-15 18:55:58 +0300

    Update add_to_cart.php

--- project/php/add_to_cart.php
+++ project/php/add_to_cart.php
@@ -1,11 +1,11 @@
 <?php
-include ("database.php");
+include("database.php");
 // Retrieve the item details from the query parameters
 $name = $_GET['name'];
 session_start();
 
-//cauta id-ul clientului
-$sql = "SELECT * FROM Clienti WHERE referer=? and cookie_id=?";
+// Search for the client's ID
+$sql = "SELECT * FROM Clienti WHERE referer=? AND cookie_id=?";
 $stmt = mysqli_prepare($conn, $sql);
 $referer = $_SESSION['my_referer'];
 $clientID = $_COOKIE['client_id'];
@@ -15,27 +15,30 @@ $result = mysqli_stmt_get_result($stmt);
 $row = mysqli_fetch_assoc($result);
 
 $clientID = $row['ID'];
-echo "id client....".$clientID;
+echo "id client...." . $clientID;
 
-$sql = "SELECT * FROM comenzi where ID_Client=? and Status is NULL ";
+$sql = "SELECT * FROM `Comenzi` WHERE ID_Client=? AND Status IS NULL LIMIT 0, 25";
 $stmt4 = mysqli_prepare($conn, $sql);
 mysqli_stmt_bind_param($stmt4, "i", $clientID);
+mysqli_stmt_execute($stmt4);
 $result = mysqli_stmt_get_result($stmt4);
+$row = mysqli_fetch_assoc($result);
 
-if($result){
-    $row = mysqli_fetch_assoc($result);
-    $insertedID = $row["ID"];
-}else{
-    //adauga id-ul clientului in baza de date
+if ($row) {
+    $insertedID = $row['ID'];
+} else {
+    // Add the client's ID to the database
     $stmt1 = mysqli_prepare($conn, "INSERT INTO Comenzi (ID_Client) VALUES (?)");
     mysqli_stmt_bind_param($stmt1, "i", $clientID);
     mysqli_stmt_execute($stmt1);
 
     $insertedID = mysqli_insert_id($conn);
     mysqli_stmt_close($stmt1);
-   
 }
-//cauta id-ul produsului
+
+mysqli_stmt_close($stmt4);
+
+// Search for the product's ID
 $sql = "SELECT * FROM Produse WHERE Nume = ?";
 $stmt3 = mysqli_prepare($conn, $sql);
 mysqli_stmt_bind_param($stmt3, "s", $name);
@@ -43,20 +46,18 @@ mysqli_stmt_execute($stmt3);
 $result = mysqli_stmt_get_result($stmt3);
 $row = mysqli_fetch_assoc($result);
 
-
-//adauga produsul cerut in baza de date
-$sql = "INSERT INTO Comanda_produse (id_comanda,id_produs) values (?,?)";
+// Add the requested product to the database
+$sql = "INSERT INTO Comanda_produse (id_comanda, id_produs) VALUES (?, ?)";
 $stmt2 = mysqli_prepare($conn, $sql);
 mysqli_stmt_bind_param($stmt2, "ii", $insertedID, $row["ID"]);
 mysqli_stmt_execute($stmt2);
 
 
-// header("Location: ../meniu.php");
-// exit();
-
+header("Location: ../meniu.php");
+exit();
+// Close all statements and the database connection
 mysqli_stmt_close($stmt);
-mysqli_stmt_close($stmt3);
-mysqli_stmt_close($stmt4);
 mysqli_stmt_close($stmt2);
+mysqli_stmt_close($stmt3);
 mysqli_close($conn);
 ?>
