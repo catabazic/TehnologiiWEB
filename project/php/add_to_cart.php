bbe554b30b50f74d831e36c2e34c04dffa1483c5
Bazic Catalina <bazic.catalina@gmail.com>
2023-06-15 15:39:13 +0300

    :anger:

--- project/php/add_to_cart.php
+++ project/php/add_to_cart.php
@@ -23,17 +23,20 @@ mysqli_stmt_bind_param($stmt4, "i", $clientID);
 $result = mysqli_stmt_get_result($stmt4);
 
 if($result){
-    $row = mysqli_fetch_assoc($result);
-    $insertedID = $row["ID"];
+    if ($row = mysqli_fetch_assoc($result)) {
+         $insertedID = $row["ID"]; 
+    }else{
+        //adauga id-ul clientului in baza de date
+        $stmt1 = mysqli_prepare($conn, "INSERT INTO Comenzi (ID_Client) VALUES (?)");
+        mysqli_stmt_bind_param($stmt1, "i", $clientID);
+        mysqli_stmt_execute($stmt1);
+    
+        $insertedID = mysqli_insert_id($conn);
+        mysqli_stmt_close($stmt1);
+    }  
 }else{
-    //adauga id-ul clientului in baza de date
-    $stmt1 = mysqli_prepare($conn, "INSERT INTO Comenzi (ID_Client) VALUES (?)");
-    mysqli_stmt_bind_param($stmt1, "i", $clientID);
-    mysqli_stmt_execute($stmt1);
+    echo "Error: " . mysqli_error($connection);
 
-    $insertedID = mysqli_insert_id($conn);
-    mysqli_stmt_close($stmt1);
-   
 }
 //cauta id-ul produsului
 $sql = "SELECT * FROM Produse WHERE Nume = ?";
@@ -51,7 +54,7 @@ mysqli_stmt_bind_param($stmt2, "ii", $insertedID, $row["ID"]);
 mysqli_stmt_execute($stmt2);
 
 
-header("Location: ../meniu.php"); // Replace "../success.php" with the relative path to the desired page
+header("Location: ../meniu.php");
 exit();
 
 mysqli_stmt_close($stmt);
