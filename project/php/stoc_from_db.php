98e2e9e4eaa5632855fff31aeb7bddf4980ab47d
BiancaCojocarescu <biancacojocarescu@yahoo.com>
2023-06-17 14:21:56 +0300

    etrag tot din baza de date pentru a afisa in meniu (sa nu mai fie hardcodate)

--- project/php/stoc_from_db.php
+++ project/php/stoc_from_db.php
@@ -3,23 +3,33 @@ include("database.php");
 
 session_start();
 
+// Setează setul de caractere pentru conexiunea MySQL
+mysqli_set_charset($conn, 'utf8');
+
 // Selectez toate produsele din baza de date
-$sql = "SELECT ID, Cantitate_in_stoc, Nume FROM Produse";
+$sql = "SELECT ID, Cantitate_in_stoc, Nume, Descriere, Pret FROM Produse";
 $result = mysqli_query($conn, $sql);
 
 $stoc = array();
 
 if (mysqli_num_rows($result) > 0) {
-    // Parcurg rezultatele și afișez starea de disponibilitate pentru fiecare produs
+    // Parcurg rezultatele și adaug informațiile fiecărui produs în $stoc
     while ($row = mysqli_fetch_assoc($result)) {
         $nume = $row["Nume"];
+        $descriere = $row["Descriere"];
+        $pret = $row["Pret"];
         $cantitate = $row["Cantitate_in_stoc"];
-        $stoc[$nume] = $cantitate;
+
+        $stoc[$nume] = array(
+            'descriere' => $descriere,
+            'pret' => $pret,
+            'inStoc' => ($cantitate > 0)
+        );
 
         if ($cantitate > 0) {
-            echo "Produsul \"$nume\"  este în stoc.<br>";
+            echo "Produsul \"$nume\" este în stoc. Descriere: $descriere, Preț: $pret.<br>";
         } else {
-            echo "Produsul \"$nume\"  nu este în stoc.<br>";
+            echo "Produsul \"$nume\" nu este în stoc. Descriere: $descriere, Preț: $pret.<br>";
         }
     }
 } else {
@@ -29,5 +39,7 @@ if (mysqli_num_rows($result) > 0) {
 mysqli_close($conn);
 
 // Pasăm rezultatul ca JSON pentru a putea fi accesat în JavaScript
-echo json_encode($stoc);
+echo json_encode($stoc, JSON_UNESCAPED_UNICODE);
+
+
 ?>
