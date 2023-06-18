05dc6d10eae4bd0d676c52f8182daf7aa9514984
Bazic Catalina <bazic.catalina@gmail.com>
2023-06-18 23:42:50 +0300

    :gem:

---
+++ project/admin.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html lang="ro">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Green Tea House</title>
+    <link rel="stylesheet" href="css/admin.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.0.0/fonts/remixicon.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <nav>
+            <div class="site-logo">
+                <img src="imagini/logo-teahouse.png" alt="logo" width="60" height="60">
+            </div>
+            
+        </nav>
+        <main>
+        </main>
+    </div>
+    <script src="javascript/stoc.js"></script>
+    <script>
+        fetchData();
+    </script>
+</body>
+
+</html>
